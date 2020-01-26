f54d8de7831896a173d9a182c33db5a919e086bb
Rohan Nobis <nobisro@gmail.com>
2020-01-26 17:12:34 -0500

    updating db connection

--- src/models/index.js
+++ src/models/index.js
@@ -6,8 +6,16 @@ const { Retro, Category, Board } = require('./models')
 
 const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds261430.mlab.com:61430/retroboard`
 
+const options = {
+    useNewUrlParser: true,
+    auth: {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD
+    }
+}
+
 const connectDb = () => {
-    return mongoose.connect(uri)
+    return mongoose.connect(uri, options)
 }
 
 const models = { Retro, Category, Board }
